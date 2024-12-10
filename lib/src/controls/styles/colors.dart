81d1bebcfa56c6fb49d2bef0c4258f430f153198
jessica malak <jessimalak21@gmail.com>
2024-12-10 14:36:22 -0500

    add colors class

---
+++ lib/src/controls/styles/colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/widgets.dart';
+
+class XpColors {
+  static const Color moonBlue = Color(0xff0066ff);
+  static const Color darkBlue = Color(0xff003c74);
+  static const Color lightBlue = Color(0xffbcd4f6);
+  static const Color orange = Color(0xffe68b2c);
+  static const Color lightOrange = Color(0xffffc73c);
+  static const Color transparent = Color(0x00ffffff);
+  static const Color white = Color(0xffffffff);
+  static const Color black = Color(0xff000000);
+  static const Color green = Color(0xff2ed330);
+}
+
+class XpDefaultThemeColors {
+  static const Color buttonOutlineColor = XpColors.darkBlue;
+  static const Color buttonHoverColorTop = Color(0xfffff0cf);
+  static const Color buttonHoverColorRight = Color(0xfffdd889);
+  static const Color buttonHoverColorLeft = Color(0xfffbc761);
+  static const Color buttonHoverColorBottom = Color(0xffe5a01a);
+  static const Color outLineColor = Color(0xff686868);
+}
