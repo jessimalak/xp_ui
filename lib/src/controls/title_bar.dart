382fa5acc97671dfef915b580b389dc51a3289eb
Jessica Malak <jrestrepo@medvision.com.co>
2024-12-11 14:52:17 -0500

    add maximize window listener

--- lib/src/controls/title_bar.dart
+++ lib/src/controls/title_bar.dart
@@ -7,16 +7,13 @@ import 'package:window_manager/window_manager.dart' hide TitleBarStyle;
 import 'package:xp_ui/src/styles/colors.dart';
 import 'package:xp_ui/xp_ui.dart';
 
-class TitleBar extends StatelessWidget {
+class TitleBar extends StatefulWidget {
   final String title;
   final bool showCloseButton;
   final bool showMaximizeButton;
   final bool showMinimizeButton;
   final bool showHelpButton;
   final VoidCallback? onHelpButtonPressed;
-  bool get _isDesktop =>
-      Platform.isLinux || Platform.isMacOS || Platform.isWindows;
-
   const TitleBar(this.title,
       {super.key,
       this.showCloseButton = true,
@@ -25,7 +22,37 @@ class TitleBar extends StatelessWidget {
       this.showHelpButton = false,
       this.onHelpButtonPressed});
 
-  static Future<bool> _emptyFuture() async => false;
+  @override
+  State<TitleBar> createState() => _TitleBarState();
+}
+
+class _TitleBarState extends State<TitleBar> with WindowListener {
+  bool get _isDesktop => Platform.isLinux || Platform.isMacOS || Platform.isWindows;
+  bool isMaximized = false;
+
+  @override
+  void onWindowMaximize() {
+    super.onWindowMaximize();
+    setState(() => isMaximized = true);
+  }
+
+  @override
+  void onWindowUnmaximize() {
+    super.onWindowUnmaximize();
+    setState(() => isMaximized = false);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    windowManager.addListener(this);
+  }
+
+  @override
+  void dispose() {
+    windowManager.removeListener(this);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -58,30 +85,30 @@ class TitleBar extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.fromLTRB(0, 3, 5, 3),
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Row(
-              children: [
-                SizedBox(
-                  width: Platform.isMacOS ? 72 : 3,
+            Expanded(
+              child: DragToMoveArea(
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: Platform.isMacOS ? 72 : 3,
+                    ),
+                    Text(
+                      widget.title,
+                      style: TextStyle(color: style.foregroundColor, fontSize: 13, fontFamily: 'Trebuchet'),
+                    ),
+                  ],
                 ),
-                Text(
-                  title,
-                  style: TextStyle(
-                      color: style.foregroundColor,
-                      fontSize: 13,
-                      fontFamily: 'Trebuchet'),
-                ),
-              ],
+              ),
             ),
             Row(
               children: [
-                if (showHelpButton)
+                if (widget.showHelpButton)
                   TitleBarActionButton(
                     icon: ActionButtonIcon.help,
-                    onPressed: onHelpButtonPressed,
+                    onPressed: widget.onHelpButtonPressed,
                   ),
-                if (showMinimizeButton)
+                if (widget.showMinimizeButton)
                   TitleBarActionButton(
                     icon: ActionButtonIcon.minimize,
                     onPressed: () {
@@ -89,33 +116,23 @@ class TitleBar extends StatelessWidget {
                       windowManager.minimize();
                     },
                   ),
-                if (showMaximizeButton)
-                  FutureBuilder(
-                    builder: (context, value) {
-                      return TitleBarActionButton(
-                        icon: !(value.data ?? true)
-                            ? ActionButtonIcon.maximize
-                            : ActionButtonIcon.maximized,
-                        onPressed: () async {
-                          if (!_isDesktop) return;
-                          if (!(value.data ?? true)) {
-                            await windowManager.maximize();
-                          } else {
-                            await windowManager.unmaximize();
-                          }
-                        },
-                      );
+                if (widget.showMaximizeButton)
+                  TitleBarActionButton(
+                    icon: !isMaximized ? ActionButtonIcon.maximize : ActionButtonIcon.maximized,
+                    onPressed: () async {
+                      if (!_isDesktop) return;
+                      if (!isMaximized) {
+                        await windowManager.maximize();
+                      } else {
+                        await windowManager.unmaximize();
+                      }
                     },
-                    future: !_isDesktop
-                        ? _emptyFuture()
-                        : windowManager.isMaximized(),
                   ),
-                if (showCloseButton)
+                if (widget.showCloseButton)
                   XpCloseButton(
                     onPressed: () {
                       if (!_isDesktop) return;
-                      SystemChannels.platform
-                          .invokeMethod('SystemNavigator.pop');
+                      SystemChannels.platform.invokeMethod('SystemNavigator.pop');
                     },
                   )
               ],
@@ -148,10 +165,7 @@ class _TitleBarButtonHoverState extends State<_TitleBarButtonHover> {
         },
         child: DecoratedBox(
           decoration: BoxDecoration(
-              gradient: _hover
-                  ? const RadialGradient(
-                      colors: [Color(0x6AFFFFFF), XpColors.transparent])
-                  : null),
+              gradient: _hover ? const RadialGradient(colors: [Color(0x6AFFFFFF), XpColors.transparent]) : null),
           child: widget.child,
         ));
   }
@@ -204,23 +218,12 @@ class _XpCloseButtonState extends State<XpCloseButton> {
                     offset: const Offset(1, 2),
                     color: _tap ? XpColors.darkRed : XpColors.lightRed,
                     blurRadius: 1),
-                const BoxShadow(
-                    inset: true,
-                    offset: Offset(-2, 2),
-                    color: XpColors.red,
-                    blurRadius: 1),
-                const BoxShadow(
-                    inset: true,
-                    offset: Offset(2, -2),
-                    color: XpColors.red,
-                    blurRadius: 1)
+                const BoxShadow(inset: true, offset: Offset(-2, 2), color: XpColors.red, blurRadius: 1),
+                const BoxShadow(inset: true, offset: Offset(2, -2), color: XpColors.red, blurRadius: 1)
               ],
               gradient: _tap
-                  ? const LinearGradient(
-                      colors: [XpColors.red, XpColors.darkRed])
-                  : const LinearGradient(
-                      colors: [XpColors.lightRed, XpColors.red],
-                      begin: Alignment.topLeft)),
+                  ? const LinearGradient(colors: [XpColors.red, XpColors.darkRed])
+                  : const LinearGradient(colors: [XpColors.lightRed, XpColors.red], begin: Alignment.topLeft)),
           child: _TitleBarButtonHover(
             child: Image.asset(
               ActionButtonIcon.close.assetPath,
@@ -272,39 +275,25 @@ class _TitleBarActionButtonState extends State<TitleBarActionButton> {
                 BoxShadow(
                     inset: true,
                     offset: const Offset(-1, 1),
-                    color: _tap
-                        ? titleBarStyle.backgroundShade5
-                        : titleBarStyle.backgroundShade2,
+                    color: _tap ? titleBarStyle.backgroundShade5 : titleBarStyle.backgroundShade2,
                     blurRadius: 1),
                 BoxShadow(
                     inset: true,
                     offset: const Offset(1, 2),
-                    color: _tap
-                        ? titleBarStyle.backgroundShade5
-                        : titleBarStyle.backgroundShade2,
+                    color: _tap ? titleBarStyle.backgroundShade5 : titleBarStyle.backgroundShade2,
                     blurRadius: 1),
                 BoxShadow(
                     inset: true,
                     offset: const Offset(-2, 2),
-                    color: _tap
-                        ? titleBarStyle.backgroundShade3
-                        : titleBarStyle.backgroundColor,
+                    color: _tap ? titleBarStyle.backgroundShade3 : titleBarStyle.backgroundColor,
                     blurRadius: 1),
-                BoxShadow(
-                    inset: true,
-                    offset: const Offset(2, -2),
-                    color: titleBarStyle.backgroundColor,
-                    blurRadius: 1)
+                BoxShadow(inset: true, offset: const Offset(2, -2), color: titleBarStyle.backgroundColor, blurRadius: 1)
               ],
               gradient: _tap
-                  ? LinearGradient(colors: [
-                      titleBarStyle.backgroundShade3,
-                      titleBarStyle.backgroundShade5
-                    ])
-                  : LinearGradient(colors: [
-                      titleBarStyle.backgroundShade1,
-                      titleBarStyle.backgroundShade3
-                    ], begin: Alignment.topLeft)),
+                  ? LinearGradient(colors: [titleBarStyle.backgroundShade3, titleBarStyle.backgroundShade5])
+                  : LinearGradient(
+                      colors: [titleBarStyle.backgroundShade1, titleBarStyle.backgroundShade3],
+                      begin: Alignment.topLeft)),
           child: _TitleBarButtonHover(
             child: Image.asset(
               widget.icon.assetPath,
