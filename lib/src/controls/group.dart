d2fa708e52a09e02397dcfe15625c781156bf684
jessica malak <jessimalak21@gmail.com>
2025-05-31 18:04:19 -0500

    fix options group width

--- lib/src/controls/group.dart
+++ lib/src/controls/group.dart
@@ -15,37 +15,59 @@ class Group extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = XpTheme.of(context);
+    final labelWidget = DefaultTextStyle(
+        style: TextStyle(
+            color: theme.groupTheme.labelColor ??
+                XpDefaultThemeColors.expandableTextColor,
+            fontSize: 12),
+        child: label);
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: DecoratedBox(
         decoration: BoxDecoration(
-            border: Border.all(color: theme.colorScheme.borderColor.withAlpha(128)),
+            border:
+                Border.all(color: theme.colorScheme.borderColor.withAlpha(128)),
             borderRadius: const BorderRadius.all(Radius.circular(4))),
         child: Stack(
           clipBehavior: Clip.none,
           alignment: Alignment.topLeft,
           children: [
             Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.fromLTRB(8.0, 0, 8, 8),
               child: Column(
-                spacing: 8,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisSize: MainAxisSize.min,
-                children: [const SizedBox.shrink(), ...children],
+                children: [
+                  Opacity(
+                    opacity: 0,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 8),
+                      child: labelWidget,
+                    ),
+                  ),
+                  ...children
+                      .expand((button) sync* {
+                        yield const SizedBox(
+                          width: 8,
+                          height: 8,
+                        );
+                        yield button;
+                      })
+                      .skip(1)
+                     
+                ],
               ),
             ),
             Positioned(
               left: 8,
               top: -10,
               child: DecoratedBox(
-                decoration: BoxDecoration(color: theme.colorScheme.backgroundColor),
-                child: Padding(
-                  padding: const EdgeInsets.all(4.0),
-                  child: DefaultTextStyle(
-                      style: TextStyle(color: theme.groupTheme.labelColor ?? XpDefaultThemeColors.expandableTextColor, fontSize: 12),
-                      child: label),
-                ),
-              ),
+                  decoration:
+                      BoxDecoration(color: theme.colorScheme.backgroundColor),
+                  child: Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: labelWidget,
+                  )),
             ),
           ],
         ),
